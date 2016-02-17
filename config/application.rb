0ef3972e309e20147e096afdb181bfc0fe42ced9
Jack O'Neill <joneill.tennis@gmail.com>
2016-02-17 17:24:04 -0500

    la

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module DfsEsport
   class Application < Rails::Application
     config.time_zone = 'Eastern Time (US & Canada)'
-    config.active_record.default_timezone = 'Eastern Time (US & Canada)'
+    config.active_record.default_timezone = :local
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
