55f72e4155473847221becb024eba68ce4ed885d
Jack O'Neill <joneill.tennis@gmail.com>
2016-02-17 17:34:57 -0500

    a

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,7 @@ Bundler.require(*Rails.groups)
 
 module DfsEsport
   class Application < Rails::Application
-    config.time_zone = 'Eastern Time (US & Canada)'
-    config.active_record.default_timezone = :local
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -17,6 +16,7 @@ module DfsEsport
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Eastern Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
