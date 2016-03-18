095704da4a8aa1b8832954de89f020fac5f3f983
Jack O'Neill <joneill.tennis@gmail.com>
2016-03-17 22:26:03 -0400

    a

--- app/controllers/deposits_controller.rb
+++ app/controllers/deposits_controller.rb
@@ -35,7 +35,7 @@ class DepositsController < ApplicationController
   end
 
   def verify
-    deposit = Deposit.where(user_id: current_user.id).where(completed: false).where(payment_id: params[:paymentId])
+    deposit = Deposit.where(user_id: current_user.id).where(completed: false).where(payment_id: params[:paymentId]).take
     @payment = PayPal::SDK::REST::Payment.find(params[:paymentId])
     if @payment.execute( :payer_id => "#{params[:payerId]}" )
       #PAYMENT WILL ONLY EXECUTE IF IT IS APPROVED ON PAYPALS END
