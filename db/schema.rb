e8bdae55da9043fb30e26b03a22b3008fb70b814
Jack O'Neill <joneill.tennis@gmail.com>
2016-04-09 17:01:34 -0400

    a

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160409204038) do
+ActiveRecord::Schema.define(version: 20160409210103) do
 
   create_table "balances", force: :cascade do |t|
     t.integer  "user_id"
@@ -69,13 +69,6 @@ ActiveRecord::Schema.define(version: 20160409204038) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "game"
-  end
-
-  create_table "notifications", force: :cascade do |t|
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "user_id"
-    t.text     "description"
   end
 
   create_table "players", force: :cascade do |t|
